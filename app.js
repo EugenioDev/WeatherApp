b536720d787b3b2390edfcfa027e26eed80819bf
Eugenio Marino <geggiodev@Air-di-Eugenio.homenet.telecomitalia.it>
2021-11-07 15:00:57 +0100

    Adding Icon

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const climatic = document.querySelector(".weather");
 const appID = "191789f7840621a53dc7ca630cd9889f";
 const inputFiled = document.getElementById("input-field");
 const form = document.querySelector("#form");
+const icon = document.querySelector('.icon')
 
 let inputValue;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   inputValue = inputFiled.value;
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${inputValue},it&units=metric&lang=it&appid=${appID}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&lang=it&appid=${appID}`
   )
     .then((response) => response.json())
     .then((response) => {
@@ -18,6 +19,12 @@ form.addEventListener("submit", (e) => {
       locationZone.textContent = response.name;
       temp.textContent = Math.floor(response.main.temp) + "°";
       climatic.textContent = response.weather[0].description;
+
+      icon.innerHTML = `
+        <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png"/>
+        `
+
+
 
       const today = document.querySelector(".day");
       let utcSeconds = response.dt;
